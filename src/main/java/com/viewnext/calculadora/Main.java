225f3c809305983e144e632b6a429e573e6dbbb9
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-02 17:51:30 +0200

    Con historial (Sonarlint approved)

--- src/main/java/com/viewnext/calculadora/Main.java
+++ src/main/java/com/viewnext/calculadora/Main.java
@@ -4,7 +4,6 @@ import java.lang.System.Logger;
 import java.util.Scanner;
 
 import com.viewnext.calculadora.operacion.Operacion;
-import com.sun.tools.sjavac.Log.Level;
 import com.viewnext.calculadora.operacion.Calculadora;
 
 public class Main {
@@ -16,24 +15,23 @@ public class Main {
 			num = es.nextInt();
 			return num;
 		} catch (Exception e) {
-			throw new IllegalArgumentException();
+			throw new IllegalArgumentException("Solo caracteres numericos");
 		}
 	}
 
 	public static void menuCalculadora(Logger logger) {
-		logger.log(System.Logger.Level.INFO, "-----------------------------------------------------------------------");
-		logger.log(System.Logger.Level.INFO, "\t1-Sumar\n" + "\t2-Restar\n" + "\t3-Dividir\n" + "\t4-Multiplicar\n" + "\t5-Historial\n"
-				+ "\t-1-Salir\n");
-		logger.log(System.Logger.Level.INFO, "-----------------------------------------------------------------------");
+		logger.log(System.Logger.Level.INFO, "-----------------------------------------------------------------------\n" + "\t1-Sumar\n"
+											+ "\t2-Restar\n" + "\t3-Dividir\n" + "\t4-Multiplicar\n" + "\t5-Historial\n" + "\t-1-Salir\n"
+											+ "-----------------------------------------------------------------------");
 	}
 
 	public static void main(String[] args) {
 		Logger logger = System.getLogger("LogCalc");
 		Calculadora calculadora = new Calculadora();
 		int opc;
-		int num1 = 1;
-		int num2 = 1;
-		float resultado;
+		int num1;
+		int num2;
+		float resultado = 0;
 		boolean salirPrograma = false;
 		do {
 			menuCalculadora(logger);
@@ -44,30 +42,61 @@ public class Main {
 			} catch (IllegalArgumentException ex) {
 				opc = 0;
 			}
-
+			
 			switch (opc) {
 			case 1:
-				resultado = calculadora.suma(num1, num2);
-				calculadora.addToHistorial(new Operacion(num1, num2, resultado, '+'));
+				try {
+					logger.log(System.Logger.Level.INFO, "1mer numero");
+					num1 = pedirInt();
+					logger.log(System.Logger.Level.INFO, "2do numero");
+					num2 = pedirInt();
+					resultado = calculadora.suma(num1, num2);
+					calculadora.addToHistorial(new Operacion(num1, num2, resultado, '+'));
+				}catch (IllegalArgumentException e){
+					logger.log(System.Logger.Level.ERROR, "DEBES PONER LOS DOS NUMEROS");
+				}
 				break;
 				
 			case 2:
-				resultado = calculadora.resta(num1, num2);
-				calculadora.addToHistorial(new Operacion(num1, num2, resultado, '+'));
+				try {
+					logger.log(System.Logger.Level.INFO, "1mer numero");
+					num1 = pedirInt();
+					logger.log(System.Logger.Level.INFO, "2do numero");
+					num2 = pedirInt();
+					resultado = calculadora.resta(num1, num2);
+					calculadora.addToHistorial(new Operacion(num1, num2, resultado, '-'));
+				}catch (IllegalArgumentException e){
+					logger.log(System.Logger.Level.ERROR, "DEBES PONER LOS DOS NUMEROS");
+				}
 				break;
 				
 			case 3:
-				resultado = calculadora.dividir(num1, num2);
-				calculadora.addToHistorial(new Operacion(num1, num2, resultado, '+'));
+				try {
+					logger.log(System.Logger.Level.INFO, "1mer numero");
+					num1 = pedirInt();
+					logger.log(System.Logger.Level.INFO, "2do numero");
+					num2 = pedirInt();
+					resultado = calculadora.dividir(num1, num2);
+					calculadora.addToHistorial(new Operacion(num1, num2, resultado, '/'));
+				}catch (IllegalArgumentException e){
+					logger.log(System.Logger.Level.ERROR, "DEBES PONER LOS DOS NUMEROS");
+				}
 				break;
 				
 			case 4:
-				resultado = calculadora.multiplicar(num1, num2);
-				calculadora.addToHistorial(new Operacion(num1, num2, resultado, '+'));
+				try {
+					logger.log(System.Logger.Level.INFO, "1mer numero");
+					num1 = pedirInt();
+					logger.log(System.Logger.Level.INFO, "2do numero");
+					num2 = pedirInt();
+					resultado = calculadora.multiplicar(num1, num2);
+					calculadora.addToHistorial(new Operacion(num1, num2, resultado, '*'));
+				}catch (IllegalArgumentException e){
+					logger.log(System.Logger.Level.ERROR, "DEBES PONER LOS DOS NUMEROS");
+				}
 				break;
 				
 			case 5:
-				System.out.println(calculadora.obtenerHistorial());
 				logger.log(System.Logger.Level.INFO, calculadora.obtenerHistorial());
 				break;
 
@@ -77,9 +106,13 @@ public class Main {
 				
 			default:
 				logger.log(System.Logger.Level.INFO, "Opcion no valida");
+				resultado = 0;
 				break;
 			}
-
+			
+			if (!salirPrograma) {
+				logger.log(System.Logger.Level.INFO, "El resultado de la operacion es: " + resultado);
+			}
 		} while (!salirPrograma);
 	}
 
